07c2e2f11d701bd47828c61be6308e7302ba78cc
Rohan-infinity <awsm.rohan44@gmail.com>
2024-07-30 17:21:51 +0530

    add middle ware and function to handle errors

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,7 +1,8 @@
 import User from "../Models/User.model.js";
 import bcrypt from "bcryptjs";
+import { errorHandler } from "../utils/errors.js";
 
-export const signup = async (req,res) => {
+export const signup = async (req,res , next) => {
   const { username, email, password} = req.body;
   const hashedPassword = bcrypt.hashSync(password, 10);
 
@@ -10,6 +11,6 @@ export const signup = async (req,res) => {
   await newUser.save();
   res.status(201).json({ message: "User Created Successfully"}); 
   } catch (error) {
-    res.status(500).json({messege: error.message});       
+    next(error);       
   }  
 }; 
--- backend/index.js
+++ backend/index.js
@@ -19,3 +19,14 @@ app.listen(3000, () => {
 
 app.use('/backend/user', userRoutes);
 app.use("/backend/auth", authRoutes);
+
+
+app.use((err, req, res, next) => {
+  const errorStatus = err.statuscode || 500;
+  const errorMessage = err.message || "Something went wrong";
+  return res.status(errorStatus).json({
+    success: false,
+    status: errorStatus,
+    message: errorMessage,
+  });
+})
