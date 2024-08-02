36a06865cb342c6c1cffdd71c84c66474dbde3ea
Rohan-infinity <awsm.rohan44@gmail.com>
2024-08-02 19:43:06 +0530

    create sign in route

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../Models/User.model.js";
 import bcrypt from "bcryptjs";
 import { errorHandler } from "../utils/errors.js";
+import jwt from 'jsonwebtoken';
 
 export const signup = async (req,res , next) => {
   const { username, email, password} = req.body;
@@ -14,3 +15,20 @@ export const signup = async (req,res , next) => {
     next(error);       
   }  
 }; 
+
+export const signin = async (req,res , next) => {
+  const { email, password} = req.body;
+  try {
+    const validUser = await User.findOne({email});
+    if(!validUser) return next(errorHandler(404, 'User Not Found'));
+    const validPassword = bcrypt.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(404, 'Wrong Credentials'));
+    //res.status(200).json({ _id: validUser._id, username: validUser.username, email: validUser.email});
+    const token = jwt.sign({ _id: validUser._id}, process.env.JWT_SECRET);
+    const {password: hashedPassword, ...others} = validUser._doc
+    const expiryDate = new Date(Date.now() + 30 * 60 * 60 * 24);
+    res.cookie('access_token', token, {httpOnly: true ,expires: expiryDate}).status(200).json(others);
+  } catch (error) { 
+    next(error);
+  }
+}
